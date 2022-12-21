c8cc5b381ac77688400b764b63620c492064595f
aashimgarg <18bcs001@smvdu.ac.in>
2022-12-21 20:38:57 +0530

    FIX allschemes query fetch

--- utils/fetch.ts
+++ utils/fetch.ts
@@ -85,7 +85,7 @@ export async function fetchSheets(link, aoa = true) {
 }
 
 export async function stateSchemeFetch(state = null) {
-  const stateList = await fetchQuery(
+  const stateList = await updatedFetchQuery(
     'schemeType',
     'Centrally Sponsored Scheme'
   );
