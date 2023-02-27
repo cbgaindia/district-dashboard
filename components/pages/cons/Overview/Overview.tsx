92e04f17fbc8ebf355897a1065ab26571e81a35e
Tauseef Shah <tauseefsshah@gmail.com>
2023-02-27 18:27:54 +0530

    UPDATE - Demogrpahic highlights to have commas for numeric values as per indian format

--- components/pages/cons/Overview/Overview.tsx
+++ components/pages/cons/Overview/Overview.tsx
@@ -4,18 +4,26 @@ import { SummaryCarousel } from 'components/pages/shared';
 import Snapshot from './Snapshot';
 
 const Overview = ({ stateMetadata, queryData, schemeList, data }) => {
+
+  const formatNumeric = (num) => {
+    return parseFloat(num).toLocaleString("en-US");
+  }
+
   const twoDecimals = (num) => {
     if (typeof num !== 'number' && typeof num !== 'string') {
       console.error('Input must be number r string');
     }
     const numString = typeof num === 'number' ? num.toString() : num;
+
     const decimalIndex = numString.indexOf('.');
+
     if (decimalIndex === -1) {
-      return numString;
-    } else {
-      const truncatedDecimal = numString.slice(decimalIndex + 1, decimalIndex + 3);
-      return Number(numString.slice(0, decimalIndex + 3) + truncatedDecimal).toLocaleString(undefined, {minimumFractionDigits: 0, maximumFractionDigits: 2});
+      return formatNumeric(numString);
     }
+
+    const truncatedDecimal = numString.slice(decimalIndex + 1, decimalIndex + 3);
+
+    return formatNumeric(Number(numString.slice(0, decimalIndex + 3) + truncatedDecimal).toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 2 }));
   };
 
   const summaryCards = React.useMemo(() => {
