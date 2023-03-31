5c2a40ccbdc5117ea6a0f246b2f23c3fee239003
aashim <55639487+aashimgarg@users.noreply.github.com>
2023-03-31 11:47:23 +0530

    FIX: add required factsheets link

--- config/factsheet.ts
+++ config/factsheet.ts
@@ -1,26 +1,26 @@
 export const FACTSHEET_DISTRICT = [
     {
         "district_code_lg": 129,
-        link : "http://52.212.152.131:3000/"
+        link : "https://districtfactsheets.openbudgetsindia.org/barabanki-uttar-pradesh"
     },
     {
         "district_code_lg": 378,
-        link : "http://52.212.152.131:3000/"
+        link : "https://districtfactsheets.openbudgetsindia.org/durg-chhattisgarh"
     },  
     {
         "district_code_lg": 490,
-        link : "http://52.212.152.131:3000/"
+        link : "https://districtfactsheets.openbudgetsindia.org/pune-maharashtra"
     },
     {
         "district_code_lg": 335,
-        link : "http://52.212.152.131:3000/"
+        link : "https://districtfactsheets.openbudgetsindia.org/latehar-jharkhand"
     },
     {
         "district_code_lg": 353,
-        link : "http://52.212.152.131:3000/"
+        link : "https://districtfactsheets.openbudgetsindia.org/gajapati-odisha"
     },
     {
         "district_code_lg": 216,
-        link : "http://52.212.152.131:3000/"
+        link : "https://districtfactsheets.openbudgetsindia.org/saharsa-bihar"
     },
 ]
